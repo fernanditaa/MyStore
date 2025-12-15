d365333011c5acce7453105e84f22f09c1d0df42
Nihiliister <bencasti9031@gmail.com>
2025-12-15 20:29:10 -0300

    Update RetrofitClient.kt

--- app/src/main/java/com/example/mystore/data/dao/remote/RetrofitClient.kt
+++ app/src/main/java/com/example/mystore/data/dao/remote/RetrofitClient.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    private const val BASE_URL = "https://jsonplaceholder.typicode.com/"
+    private const val BASE_URL = "https://kathub-z89q.onrender.com"
     val  instance: ApiService by lazy {
         val retrofit = Retrofit.Builder()
             .baseUrl(BASE_URL)
